107f0a73a4ad7519e4904d16ed3f31213cc78b6d
LeeDonghoon <imfly7@naver.com>
2014-10-12 13:02:15 +0900

    1. Tiles2 를 이용해서 레이아웃을 구성한  spring project base 오류 해결 : controller와 service분리후 request-mapping 안되는 부분 		context-base를 service로 옮긴 후 request-mapping을 service에 해줌.

---
+++ src/main/java/com/michiget/todaygye/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.michiget.todaygye.controller;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+@Controller
+public interface HomeController {
+	
+	public String home(Locale locale, Model model) throws Exception;
+
+}
---
+++ src/main/java/com/michiget/todaygye/service/HomeService.java
@@ -0,0 +1,37 @@
+package com.michiget.todaygye.service;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.michiget.todaygye.controller.HomeController;
+
+@Service
+public class HomeService implements HomeController{
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(HomeController.class);
+	
+	@Override
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
+				DateFormat.LONG, locale);
+
+		String formattedDate = dateFormat.format(date);
+
+		model.addAttribute("serverTime", formattedDate);
+
+		return "layout-tiles";
+	}
+}
